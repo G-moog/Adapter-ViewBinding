961a99f47ef0858fdb562dda4ce1789c4068dbb1
강경묵 <kingmumuop@gmail.com>
2024-03-11 08:20:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/adapterviewbinding/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.adapterviewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// ViewBinding DataBinding
+// RecyclerView Adpater findViewById
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
